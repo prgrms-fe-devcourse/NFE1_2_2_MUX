285e9da98e22143f2568ec620a6c294c8af65619
HyunbinKim <hyunbin78@naver.com>
2024-09-30 01:31:32 +0900

    #14 [feat]: 포스트 제목 입력란 기능 구현

--- src/components/modals/PostUpload.jsx
+++ src/components/modals/PostUpload.jsx
@@ -2,12 +2,25 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const PostUpload = () => {
-    
+  const [albumData, setAlbumData] = useState(null);
   const [description, setDescription] = useState('');
+  const [postTitle, setPostTitle] = useState('');
   const maxCharLimit = 300;
+
   return (
     <PostUploadContainer>
       <ContentWrapper>
+
+          <TrackDetails>
+            {/* 포스트 제목 필드와 인풋박스 */}
+            <PostTitleWrapper>
+              <PostTitleInput
+                type="text"
+                placeholder="포스트 제목을 입력해주세요"
+                value={postTitle}
+                onChange={(e) => setPostTitle(e.target.value)} />
+            </PostTitleWrapper>
+          </TrackDetails>
 
         {/* 설명 입력 섹션 */}
         <DescriptionSection>
@@ -16,11 +29,11 @@ const PostUpload = () => {
             placeholder="이 곡의 특별함은 무엇인가요?"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            maxLength={maxCharLimit}
-          />
-          <CharCount>{description.length}/{maxCharLimit}자</CharCount>
+            maxLength={maxCharLimit}/>
+          <CharCount>
+            {description.length}/{maxCharLimit}자
+          </CharCount>
         </DescriptionSection>
-
       </ContentWrapper>
     </PostUploadContainer>
   );
@@ -50,8 +63,29 @@ const ContentWrapper = styled.div`
   min-height: 60vh;
 `;
 
+const TrackDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`;
+
+const PostTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+const PostTitleInput = styled.input`
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 100%;
+`;
+
 const DescriptionSection = styled.div`
-  background-color: #C0AFE2;
+  background-color: #c0afe2;
   padding: 15px;
   border-radius: 10px;
   border: 1px solid #d1c4e9;
@@ -71,7 +105,7 @@ const DescriptionInput = styled.textarea`
   padding: 10px;
   border: none;
   border-radius: 5px;
-  background-color: #C0AFE2;
+  background-color: #c0afe2;
   font-size: 14px;
   color: white;
   resize: none;
@@ -87,4 +121,3 @@ const CharCount = styled.p`
   text-align: right;
   margin-top: 5px;
 `;
-
