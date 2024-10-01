f2b37bd6c9f778b28dfb91fc36c97ac651f54745
HyunbinKim <hyunbin78@naver.com>
2024-10-01 18:34:19 +0900

    #15 [feat]: 업로드 할 음악 파일을 선택할 수 있는 버튼 구현

--- src/components/modals/TrackUpload.jsx
+++ src/components/modals/TrackUpload.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const TrackUpload = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+  return (
+    <TrackUploadContent>
+            <>
+              <UploadButton htmlFor="fileUpload">
+                또는 이 버튼을 클릭하여 음원을 선택해주세요.
+              </UploadButton>
+              <input
+                id="fileUpload"
+                type="file"
+                style={{ display: 'none' }}
+              />
+            </>
+    </TrackUploadContent>
+  );
 };
 
 export default TrackUpload;
+
+// Styled components
+
+const TrackUploadContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  caret-color: transparent;
+`;
+
+const UploadButton = styled.label`
+  background-color: #bf94e4;
+  color: white;
+  padding: 20px 40px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70%;
+`;
