ae9f66cf6802eeab54c912b99f7b7a5121286999
jny4867 <jny200067@gmail.com>
2024-10-02 23:55:05 +0900

    #77[feat]: 스타일 적용

--- src/profile/ProfilePage.jsx
+++ src/profile/ProfilePage.jsx
@@ -39,37 +39,39 @@ const ProfilePage = ({ user, isMyPage }) => {
   }
 
   return (
-    <Container>
-      <Header>
-        <ProfileImage src={user?.image || defaultProfileImage} alt="프로필" />
-        <ProfileInfo>
-          <h1>{isMyPage ? '내 프로필' : `${user.fullName.nickName}의 프로필`}</h1>
-          <p>닉네임: {user.fullName.nickName}</p>
-          <p>소개: {user.fullName.bio}</p>
-        </ProfileInfo>
-      </Header>
-      
-      <Content>
-        <PostSection>
-          <h2>추천 포스트</h2>
-          {error && <p>{error}</p>} {/* 에러 메시지 표시 */}
-          <div>
-            {posts.length > 0 ? (
-              posts.map((post) => (
-                <PostCard key={post._id} post={post} />
-              ))
-            ) : (
-              <p>포스트가 없습니다.</p>
-            )}
-          </div>
-        </PostSection>
+  <Container>
+    <Header>
+      <ProfileImage src={user?.image || defaultProfileImage} alt="프로필" />
+      <ProfileInfo>
+        <h2>{user.fullName.nickName}</h2>
+        <Role><div>{user.role}</div></Role>
+        <Bio>
+          <p>{user.fullName.bio}</p>
+        </Bio>
+      </ProfileInfo>
+    </Header>
 
-        <MusicSection>
-          <h2>음원 목록</h2>
-          {/* 음원 목록을 여기에 추가 */}
-        </MusicSection>
-      </Content>
-    </Container>
+    <Content>
+      <PostSection>
+        <h2>{user.fullName.nickName}의 추천 포스트</h2>
+        {error && <p>{error}</p>} {/* 에러 메시지 표시 */}
+        <div>
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <PostCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p>포스트가 없습니다.</p>
+          )}
+        </div>
+      </PostSection>
+      <Separator><div></div></Separator>
+      <MusicSection>
+        <h2>{user.fullName.nickName}의 음원</h2>
+        {/* 음원 목록을 여기에 추가 */}
+      </MusicSection>
+    </Content>
+  </Container>
   );
 };
 
@@ -86,39 +88,109 @@ const Container = styled.div`
 const Header = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: 30px;
+  margin-bottom: 20px;
+  background: linear-gradient(135deg, #dac8e6, #e2a1f5);
+  color: #000000;
+  border-radius: 10px;
 `;
 
 const ProfileImage = styled.img`
-  width: 150px;
-  height: 150px;
+  width: 160px;
+  height: 160px;
   border-radius: 50%;
   object-fit: cover;
-  margin-right: 20px;
+  margin: 20px;
 `;
 
 const ProfileInfo = styled.div`
-  h1 {
-    font-size: 2rem;
-    margin-bottom: 10px;
+  align-items: center;
+  h2 {
+    font-size: 1.3rem;
+    font-weight: 600;
+    margin: 3px 3px;
   }
-  
+`;
+
+const Role = styled.div`
+  text-align: center;
+  font-weight: 700;
+
+  div {
+    width: 120px;
+    display: flex;
+    background: #DBEDED;
+    border-radius: 35px;
+    font-size: 12px;
+    color: #474150;
+    justify-content: center;
+    margin-top: 5px;
+    padding: 2px;
+    border: 0.7px solid black;
+  }
+`;
+
+const Bio = styled.div`
+  background-color: #ffffff;
+  width: 300px;
+  height: 80px;
+  border-radius: 10px;
+
   p {
-    font-size: 1.2rem;
-    margin: 5px 0;
+    font-size: 0.9rem;
+    padding: 10px;
+    font-weight: 400;
+    margin-top: 10px;
   }
 `;
 
 const Content = styled.div`
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const PostSection = styled.div`
-  flex: 1;
-  margin-right: 20px;
+  flex: 2;
+  padding-right: 20px; /* 선과 포스트 사이 간격 */
+  position: relative;
+
+  h2 {
+    font-size: 20px;
+    font-weight: 400;
+    border-bottom: 1px solid #000000;
+    padding-bottom: 10px;
+    margin-bottom: 20px;
+  }
 `;
 
 const MusicSection = styled.div`
   flex: 1;
+  min-width: 250px;
+  padding-left: 20px; /* 선과 음원 텍스트 사이 간격 추가 */
+
+  h2 {
+    font-size: 20px;
+    font-weight: 400;
+    border-bottom: 1px solid #000000;
+    padding-bottom: 10px;
+    margin-bottom: 20px;
+  }
+
+  @media (max-width: 768px) {
+    padding-left: 0; /* 화면이 줄어들 때 간격 제거 */
+    border-left: none; /* 화면이 작아질 때 선 제거 */
+  }
+`;
+
+const Separator = styled.div`
+  width: 1px;
+  background-color: #000000; /* 선 색상 */
+  height: auto; /* 부모 높이에 맞추기 */
+  
+  @media (max-width: 768px) {
+    display: none; /* 작은 화면에서는 선 숨기기 */
+  }
 `;
